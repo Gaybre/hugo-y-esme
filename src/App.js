64ac86d23d92a6c482264d18b952050be474b3b8
JuanGF <jugaybre@gmail.com>
2022-01-12 02:08:05 -0600

    codebase(react, babel, webpack)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='wrapper'>Hugo&Esme</h1>
+    </div>
+  )
+}
+
+export default App
