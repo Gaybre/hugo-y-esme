78cad817d2269dbd6ee1c829b18527839edac000
Gaybre <jugaybre@gmail.com>
2022-01-26 02:11:52 -0600

    update(Hero): improving hero style and responsive

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <div className='hero'>
       <div className='hero__overlay' />
       <div className='hero__body'>
-        <h1 className='hero__body--title'>Hugo & Esmeralda</h1>
+        <h1 className='hero__body--title drop-shadow'>Hugo & Esmeralda</h1>
         <img
-          className='hero__body--arrow'
+          className='hero__body--arrow drop-shadow'
           src='https://res.cloudinary.com/starlink/image/upload/v1642572096/he/arrow_ktmj9t.png'
           alt='arrow'
         />
-        <h1 className='hero__body--date'>23-Abril-2022</h1>
+        <h1 className='hero__body--date drop-shadow'>23-Abril-2022</h1>
       </div>
     </div>
   );
