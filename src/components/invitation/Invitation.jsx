373c5f3a779d7ac5e9e63072c85e7425a1e0c989
Gaybre <jugaybre@gmail.com>
2022-01-27 02:23:00 -0600

    invitation image and responsive

--- src/components/invitation/Invitation.jsx
+++ src/components/invitation/Invitation.jsx
@@ -4,7 +4,7 @@ import './invitation.css';
 const Invitation = () => {
   return (
     <div className='invitation-container'>
-      <img src='' alt='ivitación' />
+      <img src='https://res.cloudinary.com/starlink/image/upload/v1643271584/he/INVITACI%C3%93N-V2_g3vryv.png' alt='ivitación' />
     </div>
   );
 };
